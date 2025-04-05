73bf73dec044d0de0ca0c817e1a4f80ee50da1c9
Tearnote <marach5@gmail.com>
2025-04-05 14:34:15 +0100

    Open the console in debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ using namespace playnote;
 
 auto main(int, char*[]) -> int
 try {
+#if BUILD_TYPE == BUILD_DEBUG
+	sys::create_console();
+#endif
 	auto logger = s_logger.provide();
 	sys::set_thread_name("input");
 	L_INFO("{} {}.{}.{} starting up", AppTitle, AppVersion[0], AppVersion[1], AppVersion[2]);
