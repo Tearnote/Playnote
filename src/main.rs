ee043c4ab309d98b5da2f0fb708555a8c5f5ceb8
Tearnote <marach5@gmail.com>
2025-06-25 11:56:55 +0100

    Create surface from window

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,58 @@
 use std::fs::File;
 use std::sync::Arc;
+use std::thread;
+use std::panic::panic_any;
+use std::thread::JoinHandle;
 use anyhow::{Context, Result};
 use tracing::info;
 use tracing_subscriber::{prelude::*, fmt};
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+use wgpu::{BackendOptions, Backends, Instance, InstanceDescriptor, InstanceFlags};
 use winit::application::ApplicationHandler;
 use winit::dpi::PhysicalSize;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowAttributes, WindowId};
 
-#[derive(Default)]
-struct App {
-	window: Option<Window>,
+enum App {
+	Uninitialized,
+	Initialized(AppContext),
+}
+
+struct AppContext {
+	window: Arc<Window>,
+	render_thread_handle: Option<JoinHandle<()>>,
 }
 
 impl ApplicationHandler for App {
 	fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+		if matches!(self, App::Initialized(_)) { return }
+		
 		let attributes = WindowAttributes::default()
 			.with_inner_size(PhysicalSize::new(1280, 720))
 			.with_resizable(false)
 			.with_title(env!("CARGO_PKG_NAME"));
-		self.window = Some(event_loop.create_window(attributes)
+		let window = Arc::new(event_loop.create_window(attributes)
 			.context("Failed to create application window")
-			.unwrap_or_else(|e| panic!("{}", e.to_string())));
+			.unwrap_or_else(|e| panic_any(e)));
+
+		let (window_tx, window_rx) = crossbeam_channel::bounded(1);
+		window_tx.send(window.clone()).unwrap();
+
+		let render_thread_handle = thread::spawn(move || {
+			let instance = Instance::new(&InstanceDescriptor {
+				backends: Backends::PRIMARY,
+				flags: InstanceFlags::from_env_or_default(),
+				backend_options: BackendOptions::from_env_or_default(),
+			});
+			let _surface = instance.create_surface(window_rx.recv().unwrap());
+		});
+
+		let ctx = AppContext {
+			window,
+			render_thread_handle: Some(render_thread_handle),
+		};
+		*self = App::Initialized(ctx);
 	}
 
 	fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
@@ -33,6 +62,13 @@ impl ApplicationHandler for App {
 			}
 			_ => (),
 		}
+	}
+
+	fn exiting(&mut self, _event_loop: &ActiveEventLoop) {
+		let App::Initialized(ctx) = self else { return };
+		let render_thread_handle = ctx.render_thread_handle.take().unwrap();
+		render_thread_handle.join().unwrap_or_else(|e| panic_any(e));
+		*self = App::Uninitialized;
 	}
 }
 
@@ -44,7 +80,7 @@ fn main() -> Result<()> {
 
 	let event_loop = EventLoop::new().context("Failed to initialize windowing")?;
 	event_loop.set_control_flow(ControlFlow::Poll);
-	let mut app = App::default();
+	let mut app = App::Uninitialized;
 	event_loop.run_app(&mut app)?;
 
 	Ok(())
