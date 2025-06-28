cb4304ae27d919ffaae448c26aea071f7b18564c
Tearnote <marach5@gmail.com>
2025-06-28 15:38:49 +0100

    Configure and present swapchain

--- src/threads/render.rs
+++ src/threads/render.rs
@@ -1,11 +1,8 @@
 use std::sync::atomic::Ordering;
 use anyhow::{Context as AnyhowContext, Result};
 use tracing::{debug, error, info, instrument};
-use wgpu::{
-	BackendOptions, Backends, Device, DeviceDescriptor, Features, Instance, InstanceDescriptor,
-	InstanceFlags, Limits, MemoryHints, PowerPreference, Queue, RequestAdapterOptions, Surface,
-	Trace,
-};
+use wgpu::{BackendOptions, Backends, CompositeAlphaMode, Device, DeviceDescriptor, Features, Instance, InstanceDescriptor, InstanceFlags, Limits, MemoryHints, PowerPreference, PresentMode, Queue, RequestAdapterOptions, Surface, SurfaceError, TextureFormat, TextureUsages, Trace};
+use wgpu::wgt::SurfaceConfiguration;
 use winit::dpi::PhysicalSize;
 use winit::window::Window;
 use super::{ThreadShared, UserEvent};
@@ -27,7 +24,15 @@ async fn render_inner(shared: &ThreadShared) -> Result<()> {
 	let context = init_gpu(shared.window.as_ref()).await.context("GPU initialization failed")?;
 
 	while shared.running.load(Ordering::Relaxed) {
-		// let output = context.surface.get_current_texture();
+		let output = match context.surface.get_current_texture() {
+			Ok(output) => output,
+			Err(SurfaceError::Lost | SurfaceError::Outdated) => {
+				configure_surface(&context.surface, &context.device, shared.window.inner_size());
+				continue;
+			}
+			Err(e) => return Err(e.into())
+		};
+		output.present();
 	}
 	Ok(())
 }
@@ -56,6 +61,7 @@ async fn init_gpu(window: &Window) -> Result<Context> {
 		memory_hints: MemoryHints::Performance,
 		trace: Trace::Off,
 	}).await?;
+	configure_surface(&surface, &device, window.inner_size());
 	Ok(Context {
 		surface,
 		device,
@@ -63,4 +69,15 @@ async fn init_gpu(window: &Window) -> Result<Context> {
 	})
 }
 
-// fn configure_surface(surface: &Surface, size: PhysicalSize<u32>) {}
+fn configure_surface(surface: &Surface, device: &Device, size: PhysicalSize<u32>) {
+	surface.configure(device, &SurfaceConfiguration {
+		usage: TextureUsages::RENDER_ATTACHMENT,
+		format: TextureFormat::Bgra8UnormSrgb,
+		width: size.width,
+		height: size.height,
+		present_mode: PresentMode::AutoVsync,
+		desired_maximum_frame_latency: 2,
+		alpha_mode: CompositeAlphaMode::Auto,
+		view_formats: vec![TextureFormat::Bgra8UnormSrgb],
+	});
+}
