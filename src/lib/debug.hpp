5271dc9be1d225e2ee8e698ed579bca110075bc1
Tearnote <marach5@gmail.com>
2025-08-07 15:39:15 +0100

    Split lib/debug into header+impl

--- src/lib/debug.hpp
+++ src/lib/debug.hpp
@@ -7,74 +7,15 @@ Wrapper for OS-specific debugging enablement.
 */
 
 #pragma once
-#include "libassert/assert.hpp"
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <Windows.h>
-#include <string>
-#include <cstdio>
-#include <Windows.h>
-#include <fcntl.h>
-#include <io.h>
-#endif
 #include "preamble.hpp"
 
 namespace playnote::lib::dbg {
 
 // Register a handler to make all assert failures throw.
-inline void set_assert_handler()
-{
-	libassert::set_failure_handler([](auto const& info) {
-		throw runtime_error{info.to_string()};
-	});
-	libassert::set_color_scheme(libassert::color_scheme::blank);
-}
+void set_assert_handler();
 
 // Open the console window and attach standard outputs to it.
 // https://github.com/ocaml/ocaml/issues/9252#issuecomment-576383814
-inline void attach_console()
-{
-#ifdef _WIN32
-	AllocConsole();
-
-	std::freopen("CONOUT$", "w", stdout);
-	std::freopen("CONOUT$", "w", stderr);
-
-	auto fdOut = _open_osfhandle(reinterpret_cast<intptr_t>(GetStdHandle(STD_OUTPUT_HANDLE)), _O_WRONLY | _O_BINARY);
-	auto fdErr = _open_osfhandle(reinterpret_cast<intptr_t>(GetStdHandle(STD_ERROR_HANDLE)), _O_WRONLY | _O_BINARY);
-
-	if (fdOut) {
-		_dup2(fdOut, 1);
-		_close(fdOut);
-		SetStdHandle(STD_OUTPUT_HANDLE, reinterpret_cast<HANDLE>(_get_osfhandle(1)));
-	}
-	if (fdErr) {
-		_dup2(fdErr, 2);
-		_close(fdErr);
-		SetStdHandle(STD_ERROR_HANDLE, reinterpret_cast<HANDLE>(_get_osfhandle(2)));
-	}
-
-	_dup2(_fileno(fdopen(1, "wb")), _fileno(stdout));
-	_dup2(_fileno(fdopen(2, "wb")), _fileno(stderr));
-
-	std::setvbuf(stdout, nullptr, _IONBF, 0);
-	std::setvbuf(stderr, nullptr, _IONBF, 0);
-
-	// Set console encoding to UTF-8
-	SetConsoleOutputCP(65001);
-
-	// Enable ANSI color code support
-	auto out = GetStdHandle(STD_OUTPUT_HANDLE);
-	auto mode = 0ul;
-	GetConsoleMode(out, &mode);
-	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-	SetConsoleMode(out, mode);
-#endif
-}
+void attach_console();
 
 }
