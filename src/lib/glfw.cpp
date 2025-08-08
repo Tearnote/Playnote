f13e6fc8bd3537cd101ab1fef1afc1f9402d7322
Tearnote <marach5@gmail.com>
2025-08-08 22:05:03 +0100

    Split lib/glfw into header+impl

---
+++ src/lib/glfw.cpp
@@ -0,0 +1,133 @@
+/*
+This software is dual-licensed. For more details, please consult LICENSE.txt.
+Copyright (c) 2025 Tearnote (Hubert Maraszek)
+
+lib/glfw.cpp:
+Implementation file for lib/glfw.hpp.
+*/
+
+#include "lib/glfw.hpp"
+
+#include <vulkan/vulkan_core.h>
+#include "GLFW/glfw3.h"
+#include "preamble.hpp"
+#include "assert.hpp"
+#include "lib/vulkan.hpp"
+
+namespace playnote::lib::glfw {
+
+namespace detail {
+
+void set_window_user_pointer_raw(Window window, void* ptr)
+{
+	ASSERT(window);
+	glfwSetWindowUserPointer(window, ptr);
+}
+
+[[nodiscard]] auto get_window_user_pointer_raw(Window window) -> void*
+{
+	ASSERT(window);
+	return glfwGetWindowUserPointer(window);
+}
+
+void set_window_key_handler_raw(Window window, void (*func)(Window, int, int, int, int))
+{
+	ASSERT(window);
+	glfwSetKeyCallback(window, func);
+}
+
+void set_window_cursor_motion_handler_raw(Window window, void (*func)(Window, double, double))
+{
+	ASSERT(window);
+	glfwSetCursorPosCallback(window, func);
+}
+
+void set_window_mouse_button_handler_raw(Window window, void (*func)(Window, int, int, int))
+{
+	ASSERT(window);
+	glfwSetMouseButtonCallback(window, func);
+}
+
+}
+
+void register_error_handler()
+{
+	glfwSetErrorCallback([](int code, char const* str) {
+		throw runtime_error_fmt("[GLFW] Error #{}: {}", code, str);
+	});
+}
+
+void init() { glfwInit(); }
+
+void cleanup() noexcept try { glfwTerminate(); }
+catch (runtime_error const&) {}
+
+[[nodiscard]] auto time_since_init() -> nanoseconds
+try {
+	auto const time = duration<double>{glfwGetTime()};
+	return duration_cast<nanoseconds>(time);
+}
+catch (runtime_error const&) {
+	return nanoseconds{0};
+}
+
+void process_events() { glfwPollEvents(); }
+
+[[nodiscard]] auto get_window_closing_flag(Window window) -> bool
+{
+	ASSERT(window);
+	auto const result = glfwWindowShouldClose(window);
+	ASSUME(result == 0 || result == 1);
+	return result;
+}
+
+void set_window_closing_flag(Window window, bool flag_value)
+{
+	ASSERT(window);
+	glfwSetWindowShouldClose(window, flag_value);
+}
+
+void set_window_creation_hints()
+try {
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+}
+catch (runtime_error const&) {
+	ASSERT(false);
+}
+
+auto create_window(uvec2 size, string_view title) -> Window
+{
+	return ASSUME_VAL(glfwCreateWindow(size.x(), size.y(), string{title}.c_str(), nullptr, nullptr));
+}
+
+void destroy_window(Window window) noexcept
+try {
+	if (!window) return;
+	glfwDestroyWindow(window);
+}
+catch (runtime_error const&) {}
+
+[[nodiscard]] auto get_window_title(Window window) -> string_view
+{
+	ASSERT(window);
+	return glfwGetWindowTitle(window);
+}
+
+[[nodiscard]] auto get_window_framebuffer_size(Window window) -> uvec2
+{
+	ASSERT(window);
+	auto w = 0;
+	auto h = 0;
+	glfwGetFramebufferSize(window, &w, &h);
+	return uvec2{static_cast<uint>(w), static_cast<uint>(h)};
+}
+
+[[nodiscard]] auto create_window_surface(Window window, vk::Instance const& instance) -> vk::Surface
+{
+	auto result = VkSurfaceKHR{nullptr};
+	glfwCreateWindowSurface(instance, window, nullptr, &result);
+	return result;
+}
+
+}
--- src/lib/glfw.hpp
+++ src/lib/glfw.hpp
@@ -7,121 +7,86 @@ Wrapper for GLFW window and input handling.
 */
 
 #pragma once
-#include <vulkan/vulkan_core.h>
-#include "GLFW/glfw3.h"
-#include "volk.h"
 #include "preamble.hpp"
-#include "assert.hpp"
 #include "lib/vulkan.hpp"
+
+// Forward declaration
+struct GLFWwindow;
 
 namespace playnote::lib::glfw {
 
 // Opaque handle to a window.
 using Window = GLFWwindow*;
 
-// Make GLFW throw on any error. Can be called anytime.
-inline void register_error_handler()
-{
-	glfwSetErrorCallback([](int code, char const* str) {
-		throw runtime_error_fmt("[GLFW] Error #{}: {}", code, str);
-	});
+namespace detail {
+
+void set_window_user_pointer_raw(Window window, void* ptr);
+[[nodiscard]] auto get_window_user_pointer_raw(Window window) -> void*;
+void set_window_key_handler_raw(Window window, void (*func)(Window, int, int, int, int));
+void set_window_cursor_motion_handler_raw(Window window, void (*func)(Window, double, double));
+void set_window_mouse_button_handler_raw(Window window, void (*func)(Window, int, int, int));
+
 }
+
+// Make GLFW throw on any error. Can be called anytime.
+void register_error_handler();
 
 // Initialize GLFW.
 // Throws runtime_error on failure.
-inline void init() { glfwInit(); }
+void init();
 
 // Clean up GLFW.
 // Errors are ignored.
-inline void cleanup() noexcept try { glfwTerminate(); }
-catch (runtime_error const&) {}
+void cleanup() noexcept;
 
 // Return the me passed since the call to init_glfw(). If GLFW is not initialized, returns 0.
-[[nodiscard]] inline auto time_since_init() -> nanoseconds
-try {
-	auto const time = duration<double>{glfwGetTime()};
-	return duration_cast<nanoseconds>(time);
-}
-catch (runtime_error const&) {
-	return nanoseconds{0};
-}
+[[nodiscard]] auto time_since_init() -> nanoseconds;
 
 // Check for all open windows' events, and dispatch registered callbacks.
 // Throws runtime_error on failure, or if a callback throws.
-inline void process_events() { glfwPollEvents(); }
+void process_events();
 
 // Return the current value of the window's "closing" flag. This flag is set automatically
 // if the user presses the "X" in the corner, or programmatically via set_window_closing_flag.
-[[nodiscard]] inline auto get_window_closing_flag(Window window) -> bool
-{
-	ASSERT(window);
-	auto const result = glfwWindowShouldClose(window);
-	ASSUME(result == 0 || result == 1);
-	return result;
-}
+[[nodiscard]] auto get_window_closing_flag(Window window) -> bool;
 
 // Set the current value of the window's "closing" flag.
-inline void set_window_closing_flag(Window window, bool flag_value)
-{
-	ASSERT(window);
-	glfwSetWindowShouldClose(window, flag_value);
-}
+void set_window_closing_flag(Window window, bool flag_value);
 
 // Set window creation hints to the expected values.
-inline void set_window_creation_hints()
-try {
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-}
-catch (runtime_error const&) {
-	ASSERT(false);
-}
+void set_window_creation_hints();
 
 // Open a new window and return the handle. Close it once done.
 // Throws runtime_error on failure.
-inline auto create_window(uvec2 size, string_view title) -> Window
-{
-	return ASSUME_VAL(glfwCreateWindow(size.x(), size.y(), string{title}.c_str(), nullptr, nullptr));
-}
+auto create_window(uvec2 size, string_view title) -> Window;
 
 // Destroy a previously opened window.
 // Errors are ignored.
-inline void destroy_window(Window window) noexcept
-try {
-	if (!window) return;
-	glfwDestroyWindow(window);
-}
-catch (runtime_error const&) {}
+void destroy_window(Window window) noexcept;
 
 // Set the custom pointer value that gets passed to event handlers.
 template<typename T>
-void set_window_user_pointer(Window window, T* ptr)
-{
-	ASSERT(window);
-	glfwSetWindowUserPointer(window, ptr);
+void set_window_user_pointer(Window window, T* ptr) {
+	detail::set_window_user_pointer_raw(window, ptr);
 }
 
+// Retrieve the previously set custom pointer.
 template<typename T>
 [[nodiscard]] auto get_window_user_pointer(Window window) -> T*
 {
-	ASSERT(window);
-	return static_cast<T*>(glfwGetWindowUserPointer(window));
+	return static_cast<T*>(detail::get_window_user_pointer_raw(window));
 }
 
 // Return the Window's title. String is valid until it's changed by another call, or the window
 // is destroyed.
-[[nodiscard]] inline auto get_window_title(Window window) -> string_view
-{
-	ASSERT(window);
-	return glfwGetWindowTitle(window);
-}
+[[nodiscard]] auto get_window_title(Window window) -> string_view;
 
 enum class KeyCode: int {};
 
 enum class KeyAction: int {
-	Release = GLFW_RELEASE,
-	Press = GLFW_PRESS,
-	Repeat = GLFW_REPEAT,
+	Release = 0, // GLFW_RELEASE
+	Press = 1, // GLFW_PRESS
+	Repeat = 2, // GLFW_REPEAT
 };
 
 // Set the handler for keyboard inputs.
@@ -129,8 +94,7 @@ enum class KeyAction: int {
 template<callable<void(Window, int, int, int, int)> Func>
 void set_window_key_handler(Window window, Func&& func)
 {
-	ASSERT(window);
-	glfwSetKeyCallback(window, func);
+	detail::set_window_key_handler_raw(window, func);
 }
 
 // Set the handler for mouse cursor movement.
@@ -138,19 +102,18 @@ void set_window_key_handler(Window window, Func&& func)
 template<callable<void(Window, double, double)> Func>
 void set_window_cursor_motion_handler(Window window, Func&& func)
 {
-	ASSERT(window);
-	glfwSetCursorPosCallback(window, func);
+	detail::set_window_cursor_motion_handler_raw(window, func);
 }
 
 enum class MouseButton: int {
-	Left = GLFW_MOUSE_BUTTON_LEFT,
-	Right = GLFW_MOUSE_BUTTON_RIGHT,
-	Middle = GLFW_MOUSE_BUTTON_MIDDLE,
+	Left = 0, // GLFW_MOUSE_BUTTON_LEFT
+	Right = 1, // GLFW_MOUSE_BUTTON_RIGHT
+	Middle = 2, // GLFW_MOUSE_BUTTON_MIDDLE
 };
 
 enum class MouseButtonAction: int {
-	Release = GLFW_RELEASE,
-	Press = GLFW_PRESS,
+	Release = 0, // GLFW_RELEASE
+	Press = 1, // GLFW_PRESS
 };
 
 // Set the handler for mouse button inputs.
@@ -158,28 +121,15 @@ enum class MouseButtonAction: int {
 template<callable<void(Window, int, int, int)> Func>
 void set_window_mouse_button_handler(Window window, Func&& func)
 {
-	ASSERT(window);
-	glfwSetMouseButtonCallback(window, func);
+	detail::set_window_mouse_button_handler_raw(window, func);
 }
 
 // Return the size of the window's framebuffer in pixels.
 // Throws runtime_error on failure.
-[[nodiscard]] inline auto get_window_framebuffer_size(Window window) -> uvec2
-{
-	ASSERT(window);
-	auto w = 0;
-	auto h = 0;
-	glfwGetFramebufferSize(window, &w, &h);
-	return uvec2{static_cast<uint>(w), static_cast<uint>(h)};
-}
+[[nodiscard]] auto get_window_framebuffer_size(Window window) -> uvec2;
 
 // Create a Vulkan surface for the window.
 // Throws runtime_error on failure.
-[[nodiscard]] inline auto create_window_surface(Window window, vk::Instance const& instance) -> vk::Surface
-{
-	auto result = VkSurfaceKHR{nullptr};
-	glfwCreateWindowSurface(instance, window, nullptr, &result);
-	return result;
-}
+[[nodiscard]] auto create_window_surface(Window window, vk::Instance const& instance) -> vk::Surface;
 
 }
