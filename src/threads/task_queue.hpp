8a8c80af83913b48f4ed1192ddd138222d0e4161
Tearnote <marach5@gmail.com>
2025-10-08 14:10:19 +0100

    Add optional task progress reporting via channel

--- src/threads/task_queue.hpp
+++ src/threads/task_queue.hpp
@@ -15,7 +15,7 @@ namespace playnote::threads {
 
 class TaskQueue {
 public:
-	template<typename T>
+	template<typename T, typename P = void>
 	class Task;
 
 	TaskQueue();
@@ -26,12 +26,16 @@ public:
 	template<typename... Args, invocable<Args...> Func>
 	auto enqueue(Func&& func, Args&&... args) -> Task<invoke_result_t<Func, Args...>>;
 
+	// An overload for functions that report progress through a channel.
+	template<typename ProgressT, typename... Args, invocable<Args..., channel<ProgressT>&> Func>
+	auto enqueue(Func&& func, Args&&... args) -> Task<invoke_result_t<Func, Args..., channel<ProgressT>&>, ProgressT>;
+
 	TaskQueue(TaskQueue const&) = delete;
 	auto operator=(TaskQueue const&) -> TaskQueue& = delete;
 	TaskQueue(TaskQueue&&) = delete;
 	auto operator=(TaskQueue&&) -> TaskQueue& = delete;
 
-	template<typename T>
+	template<typename T, typename P>
 	class Task {
 	public:
 		// Retrieve the return value or exception by blocking until the task is complete.
@@ -40,9 +44,15 @@ public:
 		// Check if the task is complete.
 		[[nodiscard]] auto is_ready() const -> bool { return fut.wait_for(0s) == future_status::ready; }
 
+		// Call the provided function on every pending progress update.
+		template<callable<void(P&&)> Func>
+			requires (!same_as<P, void>)
+		void on_progress(Func&&);
+
 	private:
 		friend class TaskQueue;
 		future<T> fut;
+		shared_ptr<channel<P>> progress;
 	};
 
 private:
@@ -56,26 +66,68 @@ auto TaskQueue::enqueue(Func&& func, Args&&... args) -> Task<invoke_result_t<Fun
 {
 	using Ret = invoke_result_t<Func, Args...>;
 
-	auto ret = promise<Ret>{};
+	auto ret_promise = promise<Ret>{};
 	auto task_stub = Task<Ret>{};
-	task_stub.fut = ret.get_future();
+	task_stub.fut = ret_promise.get_future();
 	queue.enqueue([
 		func = decay_t<Func>(func),
 		args = tuple(decay_t<Args>(args)...),
-		ret = move(ret)
+		ret_promise = move(ret_promise)
 	] mutable {
 		try {
 			if constexpr (is_void_v<Ret>) {
 				apply(func, move(args));
-				ret.set_value();
+				ret_promise.set_value();
 			} else {
-				ret.set_value(apply(func, move(args)));
+				ret_promise.set_value(apply(func, move(args)));
 			}
 		} catch (...) {
-			ret.set_exception(current_exception());
+			ret_promise.set_exception(current_exception());
 		}
 	});
 	return task_stub;
+}
+
+template<typename ProgressT, typename... Args, invocable<Args..., channel<ProgressT>&> Func>
+auto TaskQueue::enqueue(Func&& func, Args&&... args) -> Task<invoke_result_t<Func, Args..., channel<ProgressT>&>, ProgressT>
+{
+	using Ret = invoke_result_t<Func, Args..., channel<ProgressT>&>;
+
+	auto ret_promise = promise<Ret>{};
+	auto task_stub = Task<Ret, ProgressT>{};
+	task_stub.fut = ret_promise.get_future();
+	task_stub.progress = make_shared<channel<ProgressT>>();
+	queue.enqueue([
+		func = decay_t<Func>(func),
+		args = tuple(decay_t<Args>(args)...),
+		ret_promise = move(ret_promise),
+		progress = task_stub.progress
+	] mutable {
+		try {
+			auto final_args = tuple_cat(move(args), make_tuple(ref(*progress)));
+			if constexpr (is_void_v<Ret>) {
+				apply(func, move(final_args));
+				ret_promise.set_value();
+			} else {
+				ret_promise.set_value(apply(func, move(final_args)));
+			}
+		} catch (...) {
+			ret_promise.set_exception(current_exception());
+		}
+	});
+	return task_stub;
+}
+
+template<typename T, typename P>
+template<callable<void(P&&)> Func>
+	requires (!same_as<P, void>)
+void TaskQueue::Task<T, P>::on_progress(Func&& func)
+{
+	if (progress->empty()) return;
+	for (auto&& message: *progress) {
+		func(move(message));
+		if (progress->empty()) return;
+	}
 }
 
 inline TaskQueue::TaskQueue()
