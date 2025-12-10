b2c4721a98ceb8765a624a7763ab36f8148e7815
Tearnote <marach5@gmail.com>
2025-12-10 09:24:36 +0000

    Split strings into font runs

--- src/gfx/text.hpp
+++ src/gfx/text.hpp
@@ -9,6 +9,7 @@ or distributed except according to those terms.
 
 #pragma once
 #include "preamble.hpp"
+#include "preamble/algorithm.hpp"
 #include "utils/logger.hpp"
 #include "lib/harfbuzz.hpp"
 #include "lib/icu.hpp"
@@ -25,10 +26,17 @@ public:
 	void shape(id style_id, string_view text); //TODO return value
 
 private:
+	using FontRef = reference_wrapper<lib::harfbuzz::Font>;
+	using FontRefConst = reference_wrapper<lib::harfbuzz::Font const>;
+
 	Logger::Category cat;
 	lib::harfbuzz::Context ctx;
 	unordered_node_map<pair<id, int>, lib::harfbuzz::Font> fonts; // key: font id, weight
-	unordered_map<id, vector<reference_wrapper<lib::harfbuzz::Font>>> styles;
+	unordered_map<id, vector<FontRef>> styles;
+
+	using Run = pair<string_view, FontRefConst>;
+	auto itemize(string_view, span<FontRef const>) -> generator<Run>;
+	auto itemize(string&&, span<FontRef const>) -> generator<Run> = delete;
 };
 
 inline TextShaper::TextShaper(Logger::Category cat):
@@ -46,7 +54,7 @@ inline void TextShaper::load_font(id font_id, io::ReadFile&& file, initializer_l
 
 inline void TextShaper::define_style(id style_id, initializer_list<id> fonts, int weight)
 {
-	auto style_fonts = vector<reference_wrapper<lib::harfbuzz::Font>>{};
+	auto style_fonts = vector<FontRef>{};
 	style_fonts.reserve(fonts.size());
 	transform(fonts, back_inserter(style_fonts), [&](auto font_id) {
 		return ref(this->fonts.at(make_pair(font_id, weight)));
@@ -57,8 +65,52 @@ inline void TextShaper::define_style(id style_id, initializer_list<id> fonts, in
 inline void TextShaper::shape(id style_id, string_view text)
 {
 	auto const& style_fonts = styles.at(style_id);
-	for (auto cluster: lib::icu::grapheme_clusters(text))
-		TRACE_AS(cat, "Cluster: {}", cluster);
+	for (auto [run, font]: itemize(text, style_fonts))
+			TRACE_AS(cat, "Run: \"{}\"", run);
+}
+
+inline auto TextShaper::itemize(string_view text, span<FontRef const> fonts) -> generator<Run>
+{
+	auto current_font = optional<FontRef>{nullopt};
+	auto run_start = text.begin();
+
+	for (auto cluster: lib::icu::grapheme_clusters(text)) {
+		auto best_font = optional<FontRef>{nullopt};
+		for (auto font: fonts) {
+			auto font_suitable = true;
+			for (auto scalar: lib::icu::scalars(cluster)) {
+				if (!lib::harfbuzz::has_glyph(font.get(), scalar)) {
+					font_suitable = false;
+					break;
+				}
+			}
+			if (font_suitable) {
+				best_font = font;
+				break;
+			}
+		}
+
+		if (!best_font) {
+			WARN_AS(cat, "No font supports the character \"{}\"", cluster);
+			// Extend the current run; the character won't be rendered, but there's no point
+			// in splitting the run because of it.
+			continue;
+		}
+		if (!current_font) {
+			// Starting the first run
+			current_font = best_font;
+			continue;
+		}
+		if (current_font->get().get() == best_font->get().get()) continue;
+
+		// The ongoing run is over
+		co_yield {string_view{run_start, cluster.begin()}, *current_font};
+		current_font = best_font;
+		run_start = cluster.begin();
+	}
+
+	// Final run
+	if (current_font) co_yield {string_view{run_start, text.end()}, *current_font};
 }
 
 }
