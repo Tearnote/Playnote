b4e9fb868240cd440643c52d1c21dcdc703bf9b3
Tearnote <marach5@gmail.com>
2025-12-14 21:36:38 +0000

    Document lib/msdf

--- src/lib/msdf.hpp
+++ src/lib/msdf.hpp
@@ -14,26 +14,39 @@ or distributed except according to those terms.
 #include "preamble.hpp"
 #include "lib/harfbuzz.hpp"
 
+// Forward declarations
+
 namespace msdfgen {
 class FontHandle;
 };
 
 namespace playnote::lib::msdf {
 
+// The curves defining a glyph; opaque type
 using msdf_atlas::GlyphGeometry;
+
+// A font atlas holding MTSDF (MSDF in RGB, SDF in alpha) data. Constructor optionally takes in
+// the initial size of the atlas in pixels.
 using MTSDFAtlas = msdf_atlas::DynamicAtlas<
 	msdf_atlas::ImmediateAtlasGenerator<
 		float, 4, &msdf_atlas::mtsdfGenerator,
 		msdf_atlas::BitmapAtlasStorage<msdf_atlas::byte, 4>
 	>
 >;
+
+// View into the atlas' pixel array.
 using AtlasView = const_multi_array_ref<byte, 3>;
 
+// Extractor of glyph data from a font.
 class GlyphLoader {
 public:
+	// Create the loader. Font resource must exist for its entire lifetime.
+	// pixels_per_em controls the scale of extracted glyph geometry.
 	GlyphLoader(lib::harfbuzz::Font const&, float pixels_per_em);
 	~GlyphLoader() noexcept;
 
+	// Load a glyph with the specified index. Returns nullopt on error. Whitespace will return
+	// an empty, but valid glyph.
 	auto load_glyph(ssize_t) -> optional<GlyphGeometry>;
 
 	GlyphLoader(GlyphLoader const&) = delete;
@@ -47,15 +60,20 @@ private:
 	float pixels_per_em;
 };
 
+// How a glyph is positioned within the atlas.
 struct GlyphLayout {
 	AABB<float2> atlas_bounds;
-	float2 bearing;
+	float2 bearing; // position of glyph origin, relative to atlas_bounds.top_left
 };
 
+// Expand the atlas with additional glyphs. The new glyphs' atlas positions are returned,
+// with indices matching input data.
 auto add_glyphs(MTSDFAtlas&, span<GlyphGeometry>) -> vector<GlyphLayout>;
 
+// Return atlas contents as a read-only view.
 auto get_atlas_contents(MTSDFAtlas const&) -> AtlasView;
 
+// Save atlas contents to a file.
 void atlas_to_image(MTSDFAtlas const&, fs::path const&);
 
 }
