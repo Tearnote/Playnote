1e3f65b6ff141bb1d0ac69556cc2038e933cebed
Tearnote <marach5@gmail.com>
2025-12-31 19:08:13 +0000

    Happy new year!

--- src/audio/mixer.cpp
+++ src/audio/mixer.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2025 Tearnote (Hubert Maraszek)
+Copyright (c) 2026 Tearnote (Hubert Maraszek)
 
 Licensed under the Mozilla Public License Version 2.0 <LICENSE-MPL-2.0.txt
 or https://www.mozilla.org/en-US/MPL/2.0/> or the Boost Software License <LICENSE-BSL-1.0.txt
